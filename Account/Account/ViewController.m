10a8d3e3a35af9231a0cdae3f3aef7545f6ae666
mars <mars@mars.com>
2021-05-21 16:42:57 +0800

    fix:增加测试代码

--- Account/Account/ViewController.m
+++ Account/Account/ViewController.m
@@ -16,6 +16,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    btn.backgroundColor = [UIColor blackColor];
+    [btn setTitle:@"test" forState:UIControlStateNormal];
+    [self.view addSubview:btn];
 }
 
 
