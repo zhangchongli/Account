d7987ceb119052dd40b3bc0672bcbc5025435a91
dengjinan <dengjinan@bytedance.com>
2021-07-06 16:11:17 +0800

    fix:增加升级测试

--- Account/Account/AppDelegate.m
+++ Account/Account/AppDelegate.m
@@ -6,7 +6,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import <OneKit/OneKitApp.h>
 @interface AppDelegate ()
 
 @end
@@ -16,6 +16,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [OneKitApp startWithLaunchOptions:launchOptions];
     return YES;
 }
 
--- Account/Account/ViewController.m
+++ Account/Account/ViewController.m
@@ -6,9 +6,14 @@
 //
 
 #import "ViewController.h"
+#import <VEAppUpdateHelper/TTAppUpdateHelperDefault.h>
+#import "UIApplication+BTDAdditions.h"
+#import <OneKit/OKServiceCenter.h>
+#import <OneKit/OKApplicationInfo.h>
+#import <OneKit/OKServices.h>
 
-@interface ViewController ()
-
+@interface ViewController ()<TTAppUpdateDelegate>
+@property (nonatomic, strong) TTAppUpdateHelperDefault *updateHelper;
 @end
 
 @implementation ViewController
@@ -17,11 +22,33 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    UIButton *btn = [[UIButton alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
-    btn.backgroundColor = [UIColor blackColor];
-    [btn setTitle:@"test" forState:UIControlStateNormal];
-    [self.view addSubview:btn];
+    __block id<OKDeviceService> deviceService = [[OKServiceCenter sharedInstance] serviceForProtocol:@protocol(OKDeviceService)];
+    UITextField *label = [[UITextField alloc] initWithFrame:CGRectMake(100, 100, 200, 50)];
+    label.text = deviceService.deviceID;
+    [self.view addSubview:label];
+    
+    [self update];
 }
 
+- (void)update {
+    if(!self.updateHelper) {
+        __block id<OKDeviceService> deviceService = [[OKServiceCenter sharedInstance] serviceForProtocol:@protocol(OKDeviceService)];
+        OKApplicationInfo *info = [OKApplicationInfo sharedInstance];
+        TTAppUpdateHelperDefault *defaultHelper = [[TTAppUpdateHelperDefault alloc] initWithDeviceID:deviceService.deviceID channel:@"beta" aid:info.appID delegate:self];
+        self.updateHelper = defaultHelper;
+        self.updateHelper.callType = @(0);
+        self.updateHelper.city = @"Shanghai";
+        [defaultHelper startCheckVersion];
+    }
+}
 
+#pragma mark TTAppUpdateDelegate
+- (void)updateViewShouldShow:(TTAppUpdateTipView *)tipView {
+    [tipView show];
+}
+
+- (void)updateViewShouldClosed:(TTAppUpdateTipView *)tipView {
+    [tipView hide];
+    self.updateHelper = nil;
+}
 @end
